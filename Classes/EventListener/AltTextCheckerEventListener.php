135f2a98bd3b0a9e69cfd0c643d56098d61c0f66
sagitov <sagitovakhmed@mail.ru>
2026-03-10 12:31:14 +0100

    [TASK] adding referencesHaveMissingAltText methode [YODA-2800] [YODA-2808]

--- Classes/EventListener/AltTextCheckerEventListener.php
+++ Classes/EventListener/AltTextCheckerEventListener.php
@@ -14,8 +14,11 @@ declare(strict_types=1);
 namespace Leuchtfeuer\AltTextChecker\EventListener;
 
 
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Imaging\Event\ModifyIconForResourcePropertiesEvent;
 use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 
 /**
@@ -38,12 +41,35 @@ class AltTextCheckerEventListener
             return;
         }
 
+        $fileUid = $resource->getUid();
         $fileAlternativeText = $resource->getProperty('alternative');
 
-        if (!$fileAlternativeText) {
+        if (!$fileAlternativeText || $this->referencesHaveMissingAltText($fileUid)) {
             $event->setOverlayIdentifier('overlay-warning');
         }
     }
+    private function referencesHaveMissingAltText(int $fileId): bool
+    {
+        $queryBuilder = GeneralUtility::makeInstance(ConnectionPool::class)
+            ->getQueryBuilderForTable('sys_file_reference');
 
+        $count = $queryBuilder
+            ->count('uid')
+            ->from('sys_file_reference')
+            ->where(
+                $queryBuilder->expr()->eq('uid_local',
+                    $queryBuilder->createNamedParameter($fileId, Connection::PARAM_INT)),
+                $queryBuilder->expr()->eq('deleted', 0),
+                $queryBuilder->expr()->or(
+                    $queryBuilder->expr()->isNull('alternative'),
+                    $queryBuilder->expr()->eq('alternative',
+                        $queryBuilder->createNamedParameter(''))
+                )
+            )
+            ->executeQuery()
+            ->fetchOne();
+
+        return $count > 0;
+    }
 
 }

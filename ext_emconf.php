12f14eeaba6cee5e503d0d1937513eccfeacf664
sagitov <sagitovakhmed@mail.ru>
2026-03-19 11:57:00 +0100

    [TASK] refactoring composer.json and ext_emconf.php [YODA-2802]

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF['alt_text_checker'] = [
     'title' => 'Alt Text Checker',
-    'description' => '"Alternative Text Checker": Adds a warning icon to indicate that a file or some of its references do not have alternative text (Alt-Text) set.',
+    'description' => 'TYPO3 extension that adds a warning overlay icon in the File List module for files missing alternative text, and adds an Alternative text column to file reference tables in the backend.',
     'category' => 'fe',
     'author' => 'Dev Leuchtfeuer',
     'author_email' => 'dev@Leuchtfeuer.com',
